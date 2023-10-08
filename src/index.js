d9aadfe6f81b687591c60ab2f0c26f03fcf96d72
Opemipo Disu <46439130+coderoflagos@users.noreply.github.com>
2023-10-08 21:42:58 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import RealWatchSDK from 'real-watch-sdk';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const WatchPartyComponent = () => {
+  const [guestLink, setGuestLink] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const createWatchParty = async () => {
+    setIsLoading(true);
+
+    try {
+      // Initialize the RealWatchSDK with your configuration
+      const watchPartySDK = new RealWatchSDK({
+        apiKey: '1234567789', // Replace with your API key
+        // Other configuration options specific to your SDK
+      });
+
+      // Replace hostId, title, and videoUrl with actual data
+      const hostId = '123456';
+      const title = 'Movie Night';
+      const videoUrl = 'https://assets.mixkit.co/videos/preview/mixkit-software-developer-working-while-drinks-coffee-1730-large.mp4';
+
+      const generatedGuestLink = await watchPartySDK.createWatchParty(hostId, title, videoUrl);
+      setGuestLink(generatedGuestLink);
+    } catch (error) {
+      console.error('Error creating Watch Party:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Watch Party</h1>
+      <button onClick={createWatchParty} disabled={isLoading}>
+        {isLoading ? 'Creating Watch Party...' : 'Create Watch Party'}
+      </button>
+      {guestLink && (
+        <div>
+          <p>Guest Link:</p>
+          <a href={guestLink} target="_blank" rel="noopener noreferrer">
+            {guestLink}
+          </a>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WatchPartyComponent;
